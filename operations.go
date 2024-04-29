0bf82d806d29be10caf79d6b3f5a792917d67140
Kim Tao <kim.tao@mongodb.com>
2024-04-29 10:41:06 -0400

    DEVPROD-2281: support add_to_path in shell.exec

--- operations.go
+++ operations.go
@@ -61,6 +61,7 @@ type CmdExecShell struct {
 	Env                           map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
 	AddExpansionsToEnv            map[string]string `json:"add_expansions_to_env,omitempty" yaml:"add_expansions_to_env,omitempty"`
 	IncludeExpansionsInEnv        map[string]string `json:"include_expansions_in_env,omitempty" yaml:"include_expansions_inenv,omitempty"`
+	AddToPath                     []string          `json:"add_to_path,omitempty" yaml:"add_to_path,omitempty"`
 	ContinueOnError               bool              `json:"continue_on_err,omitempty" yaml:"continue_on_err,omitempty"`
 	Background                    bool              `json:"background,omitempty" yaml:"background,omitempty"`
 	Silent                        bool              `json:"silent,omitempty" yaml:"silent,omitempty"`
