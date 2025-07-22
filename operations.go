d928029ce3a4e75c608f34d3f5536176efbd223a
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-07-22 09:14:02 -0400

    DEVPROD-19801 Update GitHub module to v73

--- operations.go
+++ operations.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v73/github"
 )
 
 ////////////////////////////////////////////////////////////////////////
